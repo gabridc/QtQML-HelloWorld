040f135f4cdc31f0a39d8cc6b71bb016a2907000
Gabriel Dominguez <gabriel@gabrieldominguez.es>
2021-11-05 12:29:51 +0100

    Hello para windows

---
+++ Cpp/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include "Hello.h"
+
+
+int main(int argc, char *argv[])
+{
+
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QApplication app(argc, argv);
+    qmlRegisterType<Hello>("es.hello", 1, 0, "HelloImp");
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    engine.load(url);
+
+
+
+    return app.exec();
+}
