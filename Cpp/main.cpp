21105040eb1a7830ffed88a808e81427072d645f
Gabriel Dominguez <gabriel@gabrieldominguez.es>
2021-11-10 12:07:42 +0100

    Deploy application

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -7,13 +7,15 @@
 int main(int argc, char *argv[])
 {
 
+    Q_INIT_RESOURCE(qml);
+
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QApplication app(argc, argv);
     qmlRegisterType<Hello>("es.hello", 1, 0, "HelloImp");
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    const QUrl url(QStringLiteral("qrc:/main"));
     engine.load(url);
 
 
--- Cpp/main.qml
+++ Cpp/main.qml
@@ -1,7 +1,7 @@
 import QtQuick 2.12
 import QtQuick.Window 2.12
 import QtQuick.Dialogs 1.3
-import QtQuick.Controls 2.14
+import QtQuick.Controls 2.1
 
 
 Window {
