bbda5e6f592b11f3e9d57c85e00230573e51a839
Gabriel Dominguez <gabriel@gabrieldominguez.es>
2021-11-18 11:18:10 +0100

    Corregir qmlRegisterUncreatableType no necesarios

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -4,10 +4,6 @@
 #include <QQmlContext>
 #include "Hello.h"
 #include "TableModel.h"
-#include "RowModel.h"
-#include "CellModel.h"
-#include "CellModelfactory.h"
-#include "TextModel.h"
 
 int main(int argc, char *argv[])
 {
@@ -16,10 +12,7 @@ int main(int argc, char *argv[])
 
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     qmlRegisterUncreatableType<TableModel>("es.broker", 1, 0, "OperationsModel", "OperationsModel sholud not be created in QMl");
-    qmlRegisterUncreatableType<RowModel>("es.broker", 1, 0, "RowModel", "Operation sholud not be created in QMl");
     qmlRegisterUncreatableType<CellModel>("es.broker", 1, 0, "CellModel", "Operation sholud not be created in QMl");
-    qmlRegisterUncreatableType<CellModelFactory>("es.broker", 1, 0, "CellModelfactory", "Operation sholud not be created in QMl");
-    qmlRegisterUncreatableType<TextModel>("es.broker", 1, 0, "TextModel", "Operation sholud not be created in QMl");
 
     TableModel tableModel;
 
